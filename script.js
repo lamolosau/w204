d55141866ea15773686940004c3e6a156f13d826
lamolosau <lakeldjibril@gmail.com>
2026-04-22 14:23:42 +0200

    base du projet forum communautaire sur la mercedes w204

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(
+  "%cSTOP ! Tentative d'intrusion dans le Gateway de la Mercedes détectée...",
+  "color: red; font-size: 20px; font-weight: bold;",
+);
+console.log("On plaisante. Bienvenue dans le code source du garage de Djidji.");
