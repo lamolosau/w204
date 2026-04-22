6f789bdbae573f2b99cdde3f5e90cfde19a09596
lamolosau <lakeldjibril@gmail.com>
2026-04-22 16:38:49 +0200

    nouveaux articles

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ console.log(
   "color: red; font-size: 20px; font-weight: bold;",
 );
 console.log("On plaisante. Bienvenue dans le code source du garage de Djidji.");
+
+function showMore() {
+  const hiddenArticles = document.querySelectorAll(".hidden-article");
+  hiddenArticles.forEach((article) => {
+    article.style.display = "block";
+  });
+  document.getElementById("btn-more").style.display = "none";
+}
